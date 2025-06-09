da60982fc14c308a6af9c135d573b20fbdf69551
ashutosh gaurav <ashu00665@gmail.com>
2025-06-09 13:19:29 +0530

    Add route protection for user retrieval with JWT and admin check

--- auth-service/routes/authRoutes.js
+++ auth-service/routes/authRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { registerUser,loginUser, getUser } from "../controllers/authController.js";
+import { protect } from "../middleware/protect.js";
+import { isAdmin } from "../middleware/adminCheck.js";
 
 const router= express.Router();
 
 router.post('/register',registerUser);
 router.post('/login',loginUser);
-router.get('/users',getUser);
+router.get('/users',protect,isAdmin,getUser);
 
 export default router;
